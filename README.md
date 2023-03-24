e015eac96b4f73a6564de5719579ddbe9777c205
boachiejude <judeboachiekwame@gmail.com>
2023-03-24 10:27:07 +0000

    feat: add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+# Image Classification using AWS SageMaker
+
+Use AWS Sagemaker to train a pretrained model that can perform image classification by using the Sagemaker profiling, debugger, hyperparameter tuning and other good ML engineering practices. This can be done on either the provided dog breed classication data set or one of your choice.
+
+## Project Set Up and Installation
+
+Enter AWS through the gateway in the course and open SageMaker Studio.
+Download the starter files.
+Download/Make the dataset available.
+
+## Dataset
+
+The provided dataset is the dogbreed classification dataset which can be found in the classroom.
+The project is designed to be dataset independent so if there is a dataset that is more interesting or relevant to your work, you are welcome to use it to complete the project.
+
+### Access
+
+Upload the data to an S3 bucket through the AWS Gateway so that SageMaker has access to the data.
+
+## Hyperparameter Tuning
+
+In this experiment, we used the ResNet50 model to perform image classification. We performed a hyperparameter search to find the optimal values for the learning rate and batch size. The learning rate was searched over a continuous range from 0.001 to 0.1, while the batch size was searched over a categorical set of values including 16, 32, 64, 128, 256, and 512.</br>
+Finally, we used the best values for the learning rate and batch size to train the model for 20 epochs.
+
+![Hyperparameter Tuning](./screenshots/hpo_success.png)
+![Train Model Success](./screenshots/train_model_success.png)</br>
+
+## Debugging and Profiling
+
+**TODO**: Give an overview of how you performed model debugging and profiling in Sagemaker.
+
+### Debugging
+
+Model debugging in sagemaker is done using the smdebug library which is a part of the sagemaker python sdk. The library provides a set of hooks that can be used to capture the values of tensors at different points in the training process. The library also provides a set of rules that can be used to detect common issues in the training process.</br>
+
+
+### Profiling
+
+Profiling in sagemaker is also done using the
+
+### Results
+
+**TODO**: What are the results/insights did you get by profiling/debugging your model?
+
+**TODO** Remember to provide the profiler html/pdf file in your submission.
+
+
+## Model Deployment
+
+**TODO**: Give an overview of the deployed model and instructions on how to query the endpoint with a sample input.
+
+**TODO** Remember to provide a screenshot of the deployed active endpoint in Sagemaker.
+
+## Standout Suggestions
+
+**TODO (Optional):** This is where you can provide information about any standout suggestions that you have attempted.
---
+++ train_model.py
@@ -0,0 +1,253 @@
+"""
+Script to perform hyperparameter optimization on a model
+"""
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import argparse
+import logging
+import os
+import time
+import sys
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+import smdebug.pytorch as smd
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def test(model, test_loader, criterion, device, hook):
+    hook.set_mode(smd.modes.EVAL)
+    model.eval()
+    running_loss = 0
+    running_corrects = 0
+
+    for inputs, labels in test_loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data).item()
+
+    total_loss = running_loss / len(test_loader.dataset)
+    total_acc = running_corrects / len(test_loader.dataset)
+
+    logger.info(f"Testing Loss: {total_loss}")
+    logger.info(f"Testing Accuracy: {total_acc}")
+
+
+def train(model,
+          train_loader,
+          validation_loader,
+          criterion,
+          optimizer,
+          device,
+          hook):
+
+    epochs = 20
+    best_loss = 1e6
+    image_dataset = {'train': train_loader, 'valid': validation_loader}
+    loss_counter = 0
+    hook.set_mode(smd.modes.TRAIN)
+
+    for epoch in range(epochs):
+        for phase in ['train', 'valid']:
+            print(f"Epoch {epoch}, Phase {phase}")
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+            running_samples = 0
+
+            for step, (inputs, labels) in enumerate(image_dataset[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).item()
+                running_samples += len(inputs)
+                if running_samples % 2000 == 0:
+                    accuracy = running_corrects / running_samples
+                    print("Images [{}/{} ({:.0f}%)] Loss: {:.2f} Accuracy: {}/{} ({:.2f}%)".format(
+                            running_samples,
+                            len(image_dataset[phase].dataset),
+                            100.0 * (running_samples / len(image_dataset[phase].dataset)),
+                            loss.item(),
+                            running_corrects,
+                            running_samples,
+                            100.0 * accuracy,
+                        )
+                    )
+
+            epoch_loss = running_loss / running_samples
+            epoch_acc = running_corrects / running_samples
+
+            if phase == 'valid':
+                if epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                else:
+                    loss_counter += 1
+            logger.info('{} loss: {:.4f}, acc: {:.4f}, best loss: {:.4f}'.format(phase,
+                                                                                        epoch_loss,
+                                                                                        epoch_acc,
+                                                                                        best_loss))
+        if loss_counter == 1:
+            break
+    return model
+
+
+def net():
+    model = models.resnet50(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_features = model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 128),
+                   nn.ReLU(inplace=True),
+                   nn.Linear(128, 133))
+    return model
+
+
+def model_fn(model_dir):
+    print("In model_fn. Model directory is -")
+    print(model_dir)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = net().to(device)
+
+    with open(os.path.join(model_dir, "model.pth"), "rb") as f:
+        print("Loading the dog classifier model")
+        checkpoint = torch.load(f , map_location =device)
+        model.load_state_dict(checkpoint)
+        print('MODEL-LOADED')
+        logger.info('model loaded successfully')
+    model.eval()
+    return model
+
+
+def create_data_loaders(data, batch_size):
+    train_data_path = os.path.join(data, 'train')
+    test_data_path = os.path.join(data, 'test')
+    validation_data_path = os.path.join(data, 'valid')
+
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    train_data = torchvision.datasets.ImageFolder(
+        root=train_data_path,
+        transform=train_transform
+    )
+    train_data_loader = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    test_data = torchvision.datasets.ImageFolder(
+        root=test_data_path,
+        transform=test_transform
+    )
+    test_data_loader = torch.utils.data.DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    validation_data = torchvision.datasets.ImageFolder(
+        root=validation_data_path,
+        transform=test_transform,
+    )
+    validation_data_loader = torch.utils.data.DataLoader(
+        validation_data,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    return train_data_loader, test_data_loader, validation_data_loader
+
+
+def main(args):
+    logger.info(f'Hyperparameters are LR: {args.learning_rate}, Batch Size: {args.batch_size}')
+    logger.info(f'Data Paths: {args.data}')
+
+    model = net()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    criterion = nn.CrossEntropyLoss(ignore_index=133)
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.learning_rate)
+    train_loader, test_loader, validation_loader = create_data_loaders(args.data,
+                                                                       args.batch_size)
+    model = model.to(device)
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+
+    logger.info("Training the model")
+    tic = time.perf_counter()
+    model = train(model,
+                  train_loader,
+                  validation_loader,
+                  criterion,
+                  optimizer,
+                  device,
+                  hook)
+    toc = time.perf_counter()
+    logger.info(f"Training took {toc - tic:0.4f} seconds")
+
+    logger.info("Testing the model")
+    test(model, test_loader, criterion, device, hook)
+
+    logger.info("Saving the model")
+    torch.save(model.state_dict(), os.path.join(args.model_dir, "model.pth"))
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning-rate',
+                        type=float,
+                        default=0.001)
+    parser.add_argument('--batch-size',
+                        type=int,
+                        default=32)
+    parser.add_argument('--data', type=str,
+                        default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--model_dir',
+                        type=str,
+                        default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--output_dir',
+                        type=str,
+                        default=os.environ['SM_OUTPUT_DATA_DIR'])
+
+    args = parser.parse_args()
+    print(args)
+
+    main(args)
